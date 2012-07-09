6995865bf4289d9173886b46cf79734fc3b9ea8d
Rolando Abarca <m@rolando.cl>
2012-07-09 10:21:37 -0700

    minor updates

--- templates/spidermonkey/sfunction.c
+++ templates/spidermonkey/sfunction.c
@@ -16,7 +16,8 @@ JSBool ${signature_name}(JSContext *cx, uint32_t argc, jsval *vp)
 		#set $arg_list = ", ".join($arg_array)
 		#if str($ret_type) != "void"
 		${ret_type} ret = ${class_name}::${func_name}($arg_list);
-		jsval jsret; ${ret_type.from_native($generator, "ret", "jsret", 2)};
+		jsval jsret;
+		${ret_type.from_native($generator, "ret", "jsret", 2)};
 		JS_SET_RVAL(cx, vp, jsret);
 		#else
 		${class_name}::${func_name}($arg_list);
@@ -25,7 +26,8 @@ JSBool ${signature_name}(JSContext *cx, uint32_t argc, jsval *vp)
 	#else
 	#if str($ret_type) != "void"
 	${ret_type} ret = ${class_name}::${func_name}($arg_list);
-	jsval jsret; ${ret_type.from_native($generator, "ret", "jsret", 2)};
+	jsval jsret;
+	${ret_type.from_native($generator, "ret", "jsret", 2)};
 	JS_SET_RVAL(cx, vp, jsret);
 	#else
 	${class_name}::${func_name}($arg_list);

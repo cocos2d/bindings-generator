b36a24936bb9083d0ec0ce16c5883fba3561211a
Rolando Abarca <m@rolando.cl>
2012-07-17 09:32:56 -0700

    updates templates

--- targets/spidermonkey/common/sample.js
+++ targets/spidermonkey/common/sample.js
@@ -7,6 +7,27 @@ cc.p = function (x, y) {
 	return tmp;
 }
 
+cc.c3 = function (r, g, b) {
+	var tmp = new Uint8Array(3);
+	tmp[0] = r;
+	tmp[1] = g;
+	tmp[2] = b;
+	return tmp;
+}
+
+cc.c4 = function (r, g, b, o) {
+	var tmp = new Uint8Array(4);
+	tmp[0] = r;
+	tmp[1] = g;
+	tmp[2] = b;
+	tmp[3] = o;
+	return tmp;
+};
+
+var randColorComp = function () {
+	return Math.round(Math.random() * 255);
+};
+
 var director = cc.Director.sharedDirector();
 var audioEngine = cc.SimpleAudioEngine.sharedEngine();
 var winSize = director.getWinSize();
@@ -14,13 +35,21 @@ var winSize = director.getWinSize();
 log("size: " + winSize[0] + "," + winSize[1]);
 
 var scene = cc.Scene.create();
+var layer = cc.LayerGradient.create(cc.c4(0, 0, 0, 255), cc.c4(0, 128, 255, 255));
+scene.addChild(layer);
 
-for (var i=0; i < 100; i++) {
+for (var i=0; i < 30; i++) {
 	var sprite = cc.Sprite.create("Icon.png");
 	var pos = cc.p(Math.random() * 480, Math.random() * 320);
 	sprite.setPosition(pos);
-	scene.addChild(sprite);
+	sprite.setColor(cc.c3(randColorComp(), randColorComp(), randColorComp()));
+	layer.addChild(sprite);
 }
+
+var label = cc.LabelTTF.create("Testing Label TTF", "Marker Felt", 18.0);
+label.setPosition(cc.p(240, 300));
+
+layer.addChild(label);
 
 audioEngine.setBackgroundMusicVolume(0.5);
 audioEngine.playBackgroundMusic("bgmusic.mp3", true);

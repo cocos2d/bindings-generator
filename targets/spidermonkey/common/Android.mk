bc77918deb4b4bf40be118786d46575c0041ce87
folecr <folecr@gmail.com>
2012-07-19 14:22:01 -0700

    spidermonkey helper library ScriptingCore now has dependencies on cocos2d

--- targets/spidermonkey/common/Android.mk
+++ targets/spidermonkey/common/Android.mk
@@ -13,6 +13,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
 LOCAL_WHOLE_STATIC_LIBRARIES := spidermonkey_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_static
 
 LOCAL_LDLIBS := -landroid
 LOCAL_LDLIBS += -llog
@@ -20,3 +21,4 @@ LOCAL_LDLIBS += -llog
 include $(BUILD_STATIC_LIBRARY)
 
 $(call import-module,spidermonkey/android)
+$(call import-module,cocos2dx)

db22c5851d8e473b37264451a7700c4fd98f845d
Rolando Abarca <m@rolando.cl>
2012-08-07 16:36:37 -0700

    fixes GC problem

--- targets/spidermonkey/templates/layout_head.c
+++ targets/spidermonkey/templates/layout_head.c
@@ -56,11 +56,9 @@ static JSBool dummy_constructor(JSContext *cx, uint32_t argc, jsval *vp) {
 	HASH_FIND_INT(_js_global_type_ht, &typeId, p);
 	assert(p);
 	JSObject *_tmp = JS_NewObject(cx, p->jsclass, p->proto, p->parentProto);
-#ifdef COCOS2D_VERSION
-	JS_AddObjectRoot(cx, &_tmp);      
-#endif
 	js_proxy_t *pp;
 	JS_NEW_PROXY(pp, cobj, _tmp);
+	JS_AddObjectRoot(cx, &pp->obj);
 	JS_SET_RVAL(cx, vp, OBJECT_TO_JSVAL(_tmp));
 
 	return JS_TRUE;

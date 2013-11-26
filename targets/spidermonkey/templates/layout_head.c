e6c7016f83c9d02e0b8e784c65043369a1745680
James Chen <jianhua.chen@cocos2d-x.org>
2013-11-26 15:41:27 +0800

    Check ‘initializing’ variable in JS.

--- targets/spidermonkey/templates/layout_head.c
+++ targets/spidermonkey/templates/layout_head.c
@@ -6,25 +6,36 @@
 
 template<class T>
 static JSBool dummy_constructor(JSContext *cx, uint32_t argc, jsval *vp) {
-	TypeTest<T> t;
-#if $script_control_cpp
-	T* cobj = new T();
+    JS::RootedValue initializing(cx);
+    JSBool isNewValid = JS_TRUE;
+#if not $script_control_cpp
+    JSObject* global = ScriptingCore::getInstance()->getGlobalObject();
+	isNewValid = JS_GetProperty(cx, global, "initializing", &initializing) && JSVAL_TO_BOOLEAN(initializing);
 #end if
-	js_type_class_t *typeClass = nullptr;
-	long typeId = t.s_id();
-	auto typeMapIter = _js_global_type_map.find(typeId);
-	CCASSERT(typeMapIter != _js_global_type_map.end(), "Can't find the class type!");
-	typeClass = typeMapIter->second;
-	CCASSERT(typeClass, "The value is null.");
+	if (isNewValid)
+	{
+		TypeTest<T> t;
+		js_type_class_t *typeClass = nullptr;
+		long typeId = t.s_id();
+		auto typeMapIter = _js_global_type_map.find(typeId);
+		CCASSERT(typeMapIter != _js_global_type_map.end(), "Can't find the class type!");
+		typeClass = typeMapIter->second;
+		CCASSERT(typeClass, "The value is null.");
 
-	JSObject *_tmp = JS_NewObject(cx, typeClass->jsclass, typeClass->proto, typeClass->parentProto);
-#if $script_control_cpp
-	js_proxy_t *pp = jsb_new_proxy(cobj, _tmp);
+		JSObject *_tmp = JS_NewObject(cx, typeClass->jsclass, typeClass->proto, typeClass->parentProto);
+	#if $script_control_cpp
+		T* cobj = new T();
+		js_proxy_t *pp = jsb_new_proxy(cobj, _tmp);
+		JS_AddObjectRoot(cx, &pp->obj);
+	#end if
+		JS_SET_RVAL(cx, vp, OBJECT_TO_JSVAL(_tmp));
+		return JS_TRUE;
+	}
 
-	JS_AddObjectRoot(cx, &pp->obj);
+#if not $script_control_cpp
+    JS_ReportError(cx, "Don't use `new cc.XXX`, please use `cc.XXX.create` instead! ");
 #end if
-	JS_SET_RVAL(cx, vp, OBJECT_TO_JSVAL(_tmp));
-	return JS_TRUE;
+    return JS_FALSE;
 }
 
 static JSBool empty_constructor(JSContext *cx, uint32_t argc, jsval *vp) {

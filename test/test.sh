a0ee6b305fe652a4948b428d001c97fd23451ac0
lihex <hexuan.li@chukong-inc.com>
2014-01-21 14:50:20 +0800

    issue #3781: test.sh use the prebuilt libclang3.4 and let it generate a userconf.ini

--- test/test.sh
+++ test/test.sh
@@ -1,54 +1,21 @@
-
-# options
-
-usage(){
-cat << EOF
-usage: $0 [options]
-
-Test the C++ <-> JS bindings generator
-
-OPTIONS:
--h	this help
-
-Dependencies :
-PYTHON_BIN
-CLANG_ROOT
-NDK_ROOT
-
-Define this to run from a different directory
-CXX_GENERATOR_ROOT
-
-EOF
-}
-
-while getopts "dvh" OPTION; do
-case "$OPTION" in
-d)
-debug=1
-;;
-v)
-verbose=1
-;;
-h)
-usage
-exit 0
-;;
-esac
-done
+#!/bin/bash
+# 
+# Usage:
+#   export NDK_ROOT=/path/to/NDK-r9b
+#   ./test.sh
 
 # exit this script if any commmand fails
 set -e
 
-# find current dir
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 # read user.cfg if it exists and is readable
 
 _CFG_FILE=$(dirname "$0")"/user.cfg"
-if [ -e "$_CFG_FILE" ]
+if [ -f "$_CFG_FILE" ];
 then
-    [ -r "$_CFG_FILE" ] || die "Fatal Error: $_CFG_FILE exists but is unreadable"
-    . "$_CFG_FILE"
+    if [ ! -r "$_CFG_FILE" ]; then
+       echo "Fatal Error: $_CFG_FILE exists but is unreadable"
+       exit 1
+    fi
 fi
 
 # paths
@@ -58,18 +25,13 @@ if [ -z "${NDK_ROOT+aaa}" ]; then
     NDK_ROOT="$HOME/bin/android-ndk"
 fi
 
-LIBCLANG_PATH=
-if [ -z "${CLANG_ROOT+aaa}" ]; then
-# ... if CLANG_ROOT is not set, use build-in libclang
-    LIBCLANG_PATH="$CXX_GENERATOR_ROOT/libclang"
-else
-    LIBCLANG_PATH="$CLANG_ROOT/lib"
-fi
-
 if [ -z "${PYTHON_BIN+aaa}" ]; then
 # ... if PYTHON_BIN is not set, use "/usr/bin/python2.7"
     PYTHON_BIN="/usr/bin/python2.7"
 fi
+
+# find current dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # paths with defaults hardcoded to relative paths
 
@@ -77,12 +39,78 @@ if [ -z "${CXX_GENERATOR_ROOT+aaa}" ]; then
     CXX_GENERATOR_ROOT="$DIR/.."
 fi
 
-echo "CLANG_ROOT: $CLANG_ROOT"
-echo "NDK_ROOT: $NDK_ROOT"
-echo "CXX_GENERATOR_ROOT: $CXX_GENERATOR_ROOT"
-echo "PYTHON_BIN: $PYTHON_BIN"
 
-# Generate bindings for simpletest using Android's system headers
-echo "Generating bindings for simpletest with Android headers..."
+echo "Paths"
+echo "    NDK_ROOT: $NDK_ROOT"
+echo "    PYTHON_BIN: $PYTHON_BIN"
+echo "    CXX_GENERATOR_ROOT: $CXX_GENERATOR_ROOT"
+echo "    TO_JS_ROOT: $TO_JS_ROOT"
+
+# check NDK version, must be r9b
+if ! grep -q r9b $NDK_ROOT/RELEASE.TXT
+then
+    echo " Fatal Error: NDK r9b must be required!"
+    exit 1
+fi
+
+# check clang include path
+OS_NAME=$('uname')
+NDK_LLVM_ROOT=$NDK_ROOT/toolchains/llvm-3.3/prebuilt
+case "$OS_NAME" in
+    Darwin | darwin)
+        echo "in darwin"
+        if [ -d "$NDK_LLVM_ROOT/darwin-x86_64" ]; then
+            NDK_LLVM_ROOT=$NDK_LLVM_ROOT/darwin-x86_64
+        elif [ -d "$NDK_LLVM_ROOT/darwin-x86" ]; then
+            NDK_LLVM_ROOT=$NDK_LLVM_ROOT/darwin-x86
+        else
+            echo $NDK_LLVM_ROOT
+            echo " Fatal Error: $NDK_LLVM_ROOT doesn't contains prebuilt llvm 3.3"
+            exit 1
+        fi
+        ;;
+    Linux | linux)
+        echo "in linux"
+        if [ -d "$NDK_LLVM_ROOT/linux-x86_64" ]; then
+            NDK_LLVM_ROOT=$NDK_LLVM_ROOT/linux-x86_64
+        elif [ -d "$NDK_LLVM_ROOT/linux-x86" ]; then
+            NDK_LLVM_ROOT=$NDK_LLVM_ROOT/linux-x86
+        else
+            echo " Fatal Error: $NDK_LLVM_ROOT doesn't contains prebuilt llvm 3.3"
+            exit 1
+        fi
+        ;;
+    *)
+        echo " Fatal Error: Please run this script in linux or mac osx."
+        exit 1
+        ;;
+esac
+
+
+# write userconf.ini
+
+_CONF_INI_FILE="$PWD/userconf.ini"
+if [ -f "$_CONF_INI_FILE" ]
+then
+    rm "$_CONF_INI_FILE"
+fi
+
+_CONTENTS=""
+_CONTENTS+="[DEFAULT]"'\n'
+_CONTENTS+="androidndkdir=$NDK_ROOT"'\n'
+_CONTENTS+="clangllvmdir=$NDK_LLVM_ROOT"'\n'
+_CONTENTS+="cxxgeneratordir=$CXX_GENERATOR_ROOT"'\n'
+_CONTENTS+="extra_flags="'\n'
+
+echo 
+echo "generating userconf.ini..."
+echo ---
+echo -e "$_CONTENTS"
+echo -e "$_CONTENTS" > "$_CONF_INI_FILE"
+echo ---
+
+# Generate bindings for cocos2dx
+echo "Generating bindings for cocos2dx..."
 set -x
-LD_LIBRARY_PATH=${LIBCLANG_PATH} $PYTHON_BIN ${CXX_GENERATOR_ROOT}/generator.py ${CXX_GENERATOR_ROOT}/test/test.ini -t spidermonkey -s testandroid -o ./simple_test_bindings
+
+LD_LIBRARY_PATH=${CXX_GENERATOR_ROOT}/libclang $PYTHON_BIN ${CXX_GENERATOR_ROOT}/generator.py ${CXX_GENERATOR_ROOT}/test/test.ini -t spidermonkey -s testandroid -o ./simple_test_bindings

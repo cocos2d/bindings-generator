a299bc7c689f2239f5bfcf49bd09922873617d53
Jialong Zhai <joshua.astray.14@gmail.com>
2014-07-23 11:01:40 +0800

    Update ifunction_overloaded.c

--- targets/spidermonkey/templates/ifunction_overloaded.c
+++ targets/spidermonkey/templates/ifunction_overloaded.c
@@ -91,7 +91,7 @@ bool ${signature_name}(JSContext *cx, uint32_t argc, jsval *vp)
 #if $is_constructor
 	if (cobj) {
 		if (JS_HasProperty(cx, obj, "_ctor", &ok))
-        	ScriptingCore::getInstance()->executeFunctionWithOwner(OBJECT_TO_JSVAL(obj), "_ctor", argc, argv);
+        		ScriptingCore::getInstance()->executeFunctionWithOwner(OBJECT_TO_JSVAL(obj), "_ctor", argc, argv);
 
 		JS_SET_RVAL(cx, vp, OBJECT_TO_JSVAL(obj));
 		return true;

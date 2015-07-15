45708ca27ddd69f9aa3865a805bfcff2a88e5be7
pandamicro <lphuabin@gmail.com>
2015-07-15 14:16:08 +0800

    Fix compilation error due to target_ns missing

--- targets/spidermonkey/templates/layout_foot.c
+++ targets/spidermonkey/templates/layout_foot.c
@@ -3,6 +3,9 @@ void register_all_${prefix}(JSContext* cx, JS::HandleObject obj) {
     // Get the ns
     JS::RootedObject ns(cx);
     get_or_create_js_obj(cx, obj, "${target_ns}", &ns);
+    #else 
+    // Get the global ns
+    JS::RootedObject ns(cx, ScriptingCore::getInstance()->getGlobalObject());
     #end if
 
     #for jsclass in $sorted_classes

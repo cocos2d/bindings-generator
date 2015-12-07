dc28ec9bf0dbba4a0e9b2617261278d7f075d325
Ricardo Quesada <ricardoquesada@gmail.com>
2015-12-07 13:32:29 -0800

    singletons converted

--- targets/spidermonkey/templates/sfunction.c
+++ targets/spidermonkey/templates/sfunction.c
@@ -47,6 +47,11 @@ bool ${signature_name}(JSContext *cx, uint32_t argc, jsval *vp)
         js_type_class_t *typeClass = js_get_type_from_native<${namespaced_class_name}>(ret);
         JS::RootedObject jsret(cx, jsb_ref_autoreleased_create_jsobject(cx, ret, typeClass, "${namespaced_class_name}"));
         args.rval().set(OBJECT_TO_JSVAL(jsret));
+        #elif $func_name.startswith("getInstance") and $is_ref_class
+        auto ret = ${namespaced_class_name}::${func_name}($arg_list);
+        js_type_class_t *typeClass = js_get_type_from_native<${namespaced_class_name}>(ret);
+        JS::RootedObject jsret(cx, jsb_ref_singleton_get_or_create_jsobject(cx, ret, typeClass, "${namespaced_class_name}"));
+        args.rval().set(OBJECT_TO_JSVAL(jsret));
         #else
           #if $ret_type.is_enum
         int ret = (int)${namespaced_class_name}::${func_name}($arg_list);

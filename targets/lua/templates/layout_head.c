013f295868350147b5c3c9cf7f9e7bd5c24443bc
Xpol Wan <xpolife@gmail.com>
2016-03-21 23:34:48 +0800

    include cocos headers with path rooted form cocos directory.

--- targets/lua/templates/layout_head.c
+++ targets/lua/templates/layout_head.c
@@ -1,15 +1,18 @@
-\#include "${out_file}.hpp"
+\#include "scripting/lua-bindings/auto/${out_file}.hpp"
 #if $macro_judgement
 $macro_judgement
-#end if 
+#end if
 #for header in $headers
+    #if os.path.samefile(os.path.commonprefix([header, $search_path]), $search_path)
+\#include "${os.path.relpath(header, $search_path).replace(os.path.sep, '/')}"
+    #else
 \#include "${os.path.basename(header)}"
+    #end if
 #end for
-\#include "tolua_fix.h"
-\#include "LuaBasicConversions.h"
+\#include "scripting/lua-bindings/manual/tolua_fix.h"
+\#include "scripting/lua-bindings/manual/LuaBasicConversions.h"
 #if $cpp_headers
 #for header in $cpp_headers
 \#include "${header}"
 #end for
-#end if 
-
+#end if

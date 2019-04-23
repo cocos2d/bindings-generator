9aa8f45510535e59deaa6990152363d072e68159
Arnold <397136899@qq.com>
2019-04-23 16:57:35 +0800

    update

--- targets/lua/templates/enum.c
+++ targets/lua/templates/enum.c
@@ -3,7 +3,6 @@
 
 int lua_register_${generator.prefix}_${current_class.class_name}(lua_State* tolua_S)
 {
-    //tolua_usertype(tolua_S,"${generator.scriptname_from_native($current_class.namespaced_class_name, $current_class.namespace_name)}");
     tolua_module(tolua_S, "${current_class.class_name}", 0);
     tolua_beginmodule(tolua_S,"${current_class.class_name}");
     #for m in fields
